3068bfff2919878034993716bd0d65580b7aab3f
Nicole Aburto <n.aburto.o@gmail.com>
2025-02-28 18:26:46 +0100

    add image technologies and tools in page.tsx, update tailwind version, add global style, tailwind configuration

---
+++ components/iconCard.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+//skills imagen
+interface IconCardProps {
+  src: string;
+  alt: string;
+  label: ReactNode;
+  className?: string;
+}
+
+const IconCard: React.FC<IconCardProps> = ({ src, alt, label }) => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center border-2 rounded-lg w-[70px] h-[60px] p-4 lg:w-[160px] lg:h-[160px] border-pink-900 opacity-50 lg:hover:opacity-100 lg:transition lg:duration-300 hover:border-4">
+      <Image
+        src={src}
+        alt={alt}
+        width={60}
+        height={50}
+        className="lg:transform lg:hover:scale-110 lg:transition lg:duration-300"
+      />
+      <p className="font-thin uppercase mt-2">{label}</p>
+    </div>
+  );
+};
+
+export default IconCard;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
 
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{html,ts,js,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,11 +15,45 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+     
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(100%)" },
+        },
+      },
+      animation: {
+        marquee: "marquee 55s linear infinite", // 15s para velocidad moderada
+      },
+
       fontSize: {
-        'custom-base-title': '72px', // Define el tamaño de fuente por defecto
+        h1: ['30px', '1.2'], // Para móvil (base)
+        'h1-md': ['50px', '1.2'], // Para tablet
+        'h1-lg': ['64px', '1.2'], // Para desktop
+        h2: ['23px', '1.2'], // Para móvil (base)
+        'h2-md': ['30px', '1.2'], // Para tablet
+        'h2-lg': ['50px', '1.2'], // Para desktop
+        h3: ['15px', '1.2'], // Para móvil (base)
+        'h3-md': ['18px', '1.2'], // Para tablet
+        'h3-lg': ['20px', '1.2'], // Para desktop
+        p: ['14px', '1.5'], // Para móvil (base)
+        'p-md': ['18px', '1.5'], // Para tablet
+        'p-lg': ['23px', '1.5'], // Para desktop
+        p1: ['14px', '1.5'], // Para móvil (base)
+        'p1-md': ['15px', '1.5'], // Para tablet
+        'p1-lg': ['18px', '1.5'], // Para desktop
       },
     },
   },
   plugins: [],
+
+  
 };
+
 export default config;
+
+{/*
+   fontSize: {
+        "custom-base-title": "72px", // Define el tamaño de fuente por defecto
+      }, 
+       */}
