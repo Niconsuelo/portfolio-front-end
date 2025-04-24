a9b42ba913b416986ad69c65075ef08290f62900
Nicole Aburto <n.aburto.o@gmail.com>
2025-04-24 23:51:23 +0200

    config deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    trailingSlash: true, // Muy importante para GitHub Pages
+    output: 'export',     // Para hacer export estático
+    images: {
+      unoptimized: true,  // Evita errores si usas <Image />
+    },
+  };
+  
+  export default nextConfig;
