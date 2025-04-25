ebc14af9363de8a411e366ca8d2c9b591f9f6d3d
Nicole Aburto <n.aburto.o@gmail.com>
2025-04-25 16:28:29 +0200

    git add .

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    trailingSlash: true, // Muy importante para GitHub Pages
-    output: 'export',     // Para hacer export estático
-    images: {
-      unoptimized: true,  // Evita errores si usas <Image />
-    },
   };
   
   export default nextConfig;
